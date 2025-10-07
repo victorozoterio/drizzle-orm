5e1d48ef107b6cbdd9f6fd84f017fa1587807471
Victor Ozoterio <victorozoterio@gmail.com>
2025-10-06 21:58:54 -0300

    feat: crud simples com fastify em javascript

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 const fastify = require("fastify");
+const crypto = require("node:crypto");
 
 const server = fastify();
 
+const courses = [
+  { id: "1", title: "Curso de Node.js" },
+  { id: "2", title: "Curso de React" },
+  { id: "3", title: "Curso de React Native" },
+];
+
+server.get("/courses", () => {
+  return { courses };
+});
+
+server.get("/courses/:id", (request, reply) => {
+  const courseId = request.params.id;
+  const course = courses.find((course) => course.id === courseId);
+
+  if (course) {
+    return { course };
+  }
+
+  return reply.status(404).send();
+});
+
+server.post("/courses", (request, reply) => {
+  const courseId = crypto.randomUUID();
+  const courseTitle = request.body.title;
+
+  if (!courseTitle) {
+    return reply.status(400).send({ message: "Título obrigatório." });
+  }
+
+  courses.push({ id: courseId, title: courseTitle });
+  reply.status(201).send({ id: courseId });
+});
+
 server.listen({ port: 3333 }).then(() => {
-	console.log("HTTP server running!");
+  console.log("HTTP server running!");
 });
