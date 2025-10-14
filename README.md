c948f65700f1048ed9d3108e2ea83b8560430880
Victor Ozoterio <victorozoterio@gmail.com>
2025-10-14 08:51:26 -0300

    feat: adiciona filtros e paginação

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 <a href="./.github/fundamentals.png" target="_blank">
     <img alt="Fundamentos do Desenvolvimento de Software" src="./.github/fundamentals.png" width="100%" />    
 </a>
+
+
+# Formas de enviar dados em uma requisição
+
+- Search Params -> Usado para fazer filtros, ou seja, são parâmetro opcionais
+- Route Params -> Usado para identificar recursos, ou seja, são parâmetro obrigatórios
+- Request Body -> Usado em requisições PUT ou POST para enviar dados
+- Headers -> Usado para enviar metadados
--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import z from "zod";
+import { and, asc, ilike, SQL } from "drizzle-orm";
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -10,6 +11,11 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["courses"],
         summary: "Get all courses.",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(["id", "title"]).optional().default("id"),
+          page: z.coerce.number().optional().default(1),
+        }),
         response: {
           200: z.object({
             courses: z.array(
@@ -18,19 +24,29 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 title: z.string(),
               })
             ),
+            total: z.number(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const result = await db
-        .select({
-          id: courses.id,
-          title: courses.title,
-        })
-        .from(courses);
+      const { search, orderBy, page } = request.query;
 
-      return reply.send({ courses: result });
+      const conditions: SQL[] = [];
+      if (search) conditions.push(ilike(courses.title, `%${search}%`));
+
+      const [result, total] = await Promise.all([
+        db
+          .select({ id: courses.id, title: courses.title })
+          .from(courses)
+          .orderBy(asc(courses[orderBy]))
+          .offset((page - 1) * 1)
+          .limit(1)
+          .where(and(...conditions)),
+        db.$count(courses, and(...conditions)),
+      ]);
+
+      return reply.send({ courses: result, total });
     }
   );
 };
