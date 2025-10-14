94715d35c8d1efb58fd2a1005b29171b406e85af
Victor Ozoterio <victorozoterio@gmail.com>
2025-10-14 17:40:23 -0300

    feat: garante que o usuário não irá se inscrever no mesmo curso duas vezes

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,10 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  text,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
@@ -12,13 +18,17 @@ export const courses = pgTable("courses", {
   description: text(),
 });
 
-export const enrollments = pgTable("enrollments", {
-  id: uuid().primaryKey().defaultRandom(),
-  userId: uuid()
-    .notNull()
-    .references(() => users.id),
-  courseId: uuid()
-    .notNull()
-    .references(() => courses.id),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-});
+export const enrollments = pgTable(
+  "enrollments",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid()
+      .notNull()
+      .references(() => users.id),
+    courseId: uuid()
+      .notNull()
+      .references(() => courses.id),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  },
+  (table) => [uniqueIndex().on(table.userId, table.courseId)]
+);
