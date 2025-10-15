8f39a0d7d3933427cced860169f0bb6ed8f2b6c6
Victor Ozoterio <victorozoterio@gmail.com>
2025-10-15 10:04:34 -0300

    test: cria teste para buscar cursos filtrando pelo título

---
+++ src/routes/get-courses.test.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "vitest";
+import { randomUUID } from "node:crypto";
+import request from "supertest";
+import { server } from "../app.ts";
+import { makeCourse } from "../tests/factories/make-course.factory.ts";
+
+test("get courses", async () => {
+  await server.ready();
+
+  const titleId = randomUUID();
+  await makeCourse(titleId);
+
+  const response = await request(server.server).get(
+    `/courses?search=${titleId}`
+  );
+
+  expect(response.status).toEqual(200);
+  expect(response.body).toEqual({
+    total: 1,
+    courses: [
+      {
+        id: expect.any(String),
+        title: titleId,
+        enrollments: 0,
+      },
+    ],
+  });
+});
--- src/tests/factories/make-course.factory.ts
+++ src/tests/factories/make-course.factory.ts
@@ -2,12 +2,10 @@ import { faker } from "@faker-js/faker";
 import { db } from "../../database/client.ts";
 import { courses } from "../../database/schema.ts";
 
-export async function makeCourse() {
+export async function makeCourse(title?: string) {
   const result = await db
     .insert(courses)
-    .values({
-      title: faker.lorem.words(4),
-    })
+    .values({ title: title ?? faker.lorem.words(4) })
     .returning();
 
   return result[0];
